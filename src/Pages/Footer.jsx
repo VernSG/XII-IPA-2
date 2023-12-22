4e28f792e50936ff0128b2008411ebbb64447304
VernSG <68459009+VernSG@users.noreply.github.com>
2023-12-22 13:14:44 +0800

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 					<div class="mb-6 md:mb-0">
 						<a href="#" class="flex items-center">
 							<img
-								src="./Pages/logoanime-removebg-preview-png"
+								src="./Pages/logoanime-removebg-preview.png"
 								alt=""
 								className="h-[4.5rem] w-[4.5rem] right-2 relative rounded-full brightness-200"
 							/>
